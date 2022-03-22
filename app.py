a6d57c777fb3ba89fdc6127c1640571de4dd1bb5
saravanaselvan <spsaravanaselvan@gmail.com>
2022-03-22 17:50:34 +0530

    db init app in app py for pythonganywhere

--- app.py
+++ app.py
@@ -30,11 +30,14 @@ api.add_resource(UserRegister, '/api/register')
 api.add_resource(DownloadResult, '/api/download_result/<int:id>')
 api.add_resource(Login, '/api/login')
 
-if __name__ == "__main__":
-    db.init_app(app)
+db.init_app(app)
 
-    @app.before_first_request
-    def create_tables():
-        db.create_all()
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+if __name__ == "__main__":
 
     app.run(debug=True)
